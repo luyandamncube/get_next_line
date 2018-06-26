33a51b17f5c1e5d39016248332808edfda2c4ab4
luyandamncube <59448873@mylife.unisa.ac.za>
2018-06-26 21:42:11 +0200

    Almost working. Garbage values in leftover :/

--- get_next_line.c
+++ get_next_line.c
@@ -13,7 +13,7 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		ft_getline(int fd, void **store, void **leftover)
+int		ft_getline(int fd, char **store, char **leftover)
 {
 	char	*nlpos;
 	char	buf[BUFF_SIZE + 1];
@@ -23,12 +23,14 @@ int		ft_getline(int fd, void **store, void **leftover)
 		return (-1);
 	*store = ft_strdup("");
 	*store = ft_strcpy(*store, *leftover);
+	//*store = ft_strdup(*leftover);
 	while (0 < (rd = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[rd] = '\0';
 		if (NULL != (nlpos = ft_memchr(buf, '\n', BUFF_SIZE)))
 		{
 			*leftover = ft_strcpy(*leftover, nlpos + 1);
+			//*leftover = ft_strdup(nlpos + 1);
 			*nlpos = '\0';
 			*store = ft_strjoin(*store, buf);
 			return (ft_strlen(*store));
@@ -40,9 +42,9 @@ int		ft_getline(int fd, void **store, void **leftover)
 
 int		get_next_line(const int fd, char **line)
 {
-	void 			*leftover;
-	void			*store;
+	char 	*leftover;							//might need to be static, try static if you have memory corruption
 	int 			ret;
+	
 	if (leftover == NULL)
 	{
 		leftover = malloc(BUFF_SIZE + 1);
@@ -50,7 +52,7 @@ int		get_next_line(const int fd, char **line)
 	}
 	if (fd < 0 || BUFF_SIZE < 1)
 		return (-1);
-	if (0 < (ret = ft_getline(fd, &store, &leftover)))
+	if (0 < (ret = ft_getline(fd, line, &leftover)))
 		return (1);
 	if (ret == 0)
 		return (0);
@@ -58,33 +60,3 @@ int		get_next_line(const int fd, char **line)
 		return (-1);
 	return (0);
 }
-/*
-int     main(int argc, char **argv)
-{
-	int             fd;
-	char            *line;
-	char            *left;
-
-	if (argc != 2)
-		printf("USAGE: ./test_ filename\n");
-	else
-	{
-		printf("SUCCESS!\n");
-		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
-			printf("Error reading file\n");
-		printf("STATUS: %d\n", ft_getline(fd, &line, &left));
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-		printf("STATUS: %d\n", ft_getline(fd, &line, &left));
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-		printf("STATUS: %d\n", ft_getline(fd, &line, &left));
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-		printf("STATUS: %d\n", ft_getline(fd, &line, &left));
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-	}
-	return (0);
-}*/
--- testscript.sh
+++ testscript.sh
@@ -1,4 +1,4 @@
 make -C libft/ fclean && make -C libft/
-clang -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
-clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
-clang -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
+gcc -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
+gcc -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
+gcc -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft

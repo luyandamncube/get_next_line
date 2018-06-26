6c1b9b1b712c85f07f83e2f968d4c4271021f1ae
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-06-26 15:32:13 +0200

    Fixed test main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 15:27:36 by lmncube           #+#    #+#             */
-/*   Updated: 2018/06/25 16:27:00 by lmncube          ###   ########.fr       */
+/*   Updated: 2018/06/26 15:29:02 by lmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,35 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-	int 			fd;
-	char 			*line;
-	char 			*left;
-
+	int             fd;
+	char			*store;
 	if (argc != 2)
 		printf("USAGE: ./test_ filename\n");
 	else
 	{
-		printf("SUCCESS!\n");
 		fd = open(argv[1], O_RDONLY);
 		if (fd < 0)
 			printf("Error reading file\n");
-		printf("STATUS: %d\n", get_next_line(fd, &line);
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-		printf("STATUS: %d\n", get_next_line(fd, &line);
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-		printf("STATUS: %d\n", get_next_line(fd, &line);
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-		printf("STATUS: %d\n", get_next_line(fd, &line);
-		printf("OUTPUT: %s\n", line);
-		printf("////////// NEXT ///////////\n");
-	}	
+		else
+		{
+			printf("READ: %d\n", get_next_line(fd, &store));
+			printf("CONTENT: %s\n", store);
+			printf("////////// NEXT ///////////\n");
+			printf("READ: %d\n", get_next_line(fd, &store));
+			printf("CONTENT: %s\n", store);
+			printf("////////// NEXT ///////////\n");
+			printf("READ: %d\n", get_next_line(fd, &store));
+			printf("CONTENT: %s\n", store);
+			printf("////////// NEXT ///////////\n");
+			printf("READ: %d\n", get_next_line(fd, &store));
+			printf("CONTENT: %s\n", store);
+			printf("////////// NEXT ///////////\n");
+			printf("READ: %d\n", get_next_line(fd, &store));
+			printf("CONTENT: %s\n", store);
+			printf("////////// NEXT ///////////\n");
+		}
+	}
 	return (0);
-
 }

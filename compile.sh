28522d06b01889b1d48b192feba8ee7b40eee3e8
luyandamncube <59448873@mylife.unisa.ac.za>
2018-07-05 22:10:53 +0200

    shortened compilation

--- compile.sh
+++ compile.sh
@@ -3,3 +3,5 @@ gcc -c -Wall -Werror -Wextra get_next_line.c -ggdb3 -I ./libft
 gcc -c -Wall -Werror -Wextra main.c -I ./libft
 gcc -o test_gnl get_next_line.o main.o -L ./libft -lft
 make fclean -C ./libft
+rm main.o
+rm get_next_line.o

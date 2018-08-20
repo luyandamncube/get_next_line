7509529dd1eb9c2cd48bcdef0e061ffdbf51f9ad
luyandamncube <59448873@mylife.unisa.ac.za>
2018-08-20 21:07:58 +0200

    fixed overrun issue

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 make re -C ./libft
 gcc -c -Wall -Werror -Wextra get_next_line.c -ggdb3 -I ./libft
-gcc -c -Wall -Werror -Wextra main.c -I ./libft
-gcc -o test_gnl get_next_line.o main.o -L ./libft -lft
+gcc -c -Wall -Werror -Wextra get_from_std_io.c -I ./libft
+gcc -o test_gnl get_next_line.o get_from_std_io.o -L ./libft -lft
 make fclean -C ./libft
-rm main.o
+rm get_from_std_io.o
 rm get_next_line.o
--- get_next_line.h
+++ get_next_line.h
@@ -12,8 +12,8 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 8u
-# include "libft/includes/libft.h"
+# define BUFF_SIZE 1
+# include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 
